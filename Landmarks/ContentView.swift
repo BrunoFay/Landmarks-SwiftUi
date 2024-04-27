259c0d65a8eed5298480e5e73c32aa5fdee7636a
Bruno Alves Fay <brunofay1@hotmail.com>
2024-04-26 21:56:16 -0300

    feat: Build Leandmarks tutorial with my region

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,12 +10,34 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MapView()
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Rádio Farroupilha")
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+                HStack {
+                    Text("Ponta grossa")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("RS")
+                        .font(.subheadline)
+                }
+                Divider()
+                
+                Text("Sobre a Estação Transmissora")
+                    .font(.title2)
+                Text("A Antiga Estação Transmissora da Rádio Farroupilha, inaugurado como “Estação Transmissora PRH-2”, mais conhecida como “antiga sede da Rádio Farroupilha”, é um prédio histórico em ruínas e está localizado no bairro Ponta Grossa, em Porto Alegre.")
+                    .font(.subheadline)
+                    .foregroundColor(Color.gray)
+                    
+            }
+            .padding()
         }
-        .padding()
+        Spacer()
     }
 }
 
