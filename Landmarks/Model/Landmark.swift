b60e27ba1a04ff6a06c5446300d206ea24b0b5e8
Bruno Alves Fay <brunofay1@hotmail.com>
2024-04-27 19:22:02 -0300

    feat: Add category views and tabs menu

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -16,20 +16,28 @@ struct Landmark: Hashable, Codable, Identifiable {
     var state: String
     var description: String
     var isFavorite: Bool
+    var category: Category
+    var isFeatured: Bool
+    
+    enum Category: String, CaseIterable, Codable {
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+    }
     
     private var imageName: String
     var image: Image {
         Image(imageName)
     }
-  
+    
     struct Coordinates: Hashable, Codable {
         var latitude: Double
         var longitude: Double
     }
-
+    
     private var coordinates: Coordinates
     var locationCoodinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D( latitude: coordinates.latitude, longitude: coordinates.longitude)
     }
-  
+    
 }
--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var modelData = ModelData()
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
     
     var body: some View {
-        LandmarkList()
+        TabView () {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+            
+            
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
     }
 }
 
